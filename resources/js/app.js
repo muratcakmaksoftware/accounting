7c5707a1bcf342af7aae73cf147dd30e69b10659
Murat Çakmak <muratcakmak5252@gmail.com>
2022-10-07 19:03:34 +0300

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import './bootstrap';
+
+/**
+ * Jquery
+ */
+import '../../node_modules/jquery/dist/jquery.min';
+
+/**
+ * admin-lte
+ */
+import '../../node_modules/admin-lte/dist/js/adminlte.min';
